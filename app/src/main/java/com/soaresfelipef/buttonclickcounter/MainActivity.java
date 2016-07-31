a1743484783227bc21cd97e3290674233b0cc470
Felipe Soares <soaresfelipef@gmail.com>
2016-07-31 20:22:28 -0300

    Toast added to settings option

--- app/src/main/java/com/soaresfelipef/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/soaresfelipef/buttonclickcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast toastMessage = Toast.makeText(this, "Settings option clicked",Toast.LENGTH_LONG);
+            toastMessage.show();
             return true;
         }
 
